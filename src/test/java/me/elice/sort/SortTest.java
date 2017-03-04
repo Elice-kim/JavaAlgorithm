5316127aece6558208a0b9b40069871ad7665857
elice <grit_julia@naver.com>
2017-03-04 21:39:46 +0900

    add BubbleSort (with test code)

--- src/test/java/me/elice/sort/SortTest.java
+++ src/test/java/me/elice/sort/SortTest.java
@@ -15,4 +15,15 @@ public class SortTest {
         int[] expected = {1, 2, 3, 4, 5, 6, 9};
         assertArrayEquals(insertion.sort(input), expected);
     }
+
+    @Test
+    public void testBubbleSort() {
+        //given
+        Bubble bubble = new Bubble();
+        //when
+        int[] input = {1, 6, 2, 3, 4, 9, 5};
+        //then
+        int[] expected = {1, 2, 3, 4, 5, 6, 9};
+        assertArrayEquals(bubble.sort(input), expected);
+    }
 }
